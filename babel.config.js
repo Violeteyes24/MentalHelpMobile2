6047a353df8c29f66cc28ab1c4770585ffe01dcd
Violeteyes24 <gmzach024@gmail.com>
2025-02-02 17:30:25 +0800

    successful rule-based chatbot, just need the dynamic responses from openai

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,7 @@
-require('dotenv').config();
-
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: ['babel-preset-expo'],
-        plugins: [
-            [
-                'module:react-native-dotenv', // Enables .env support in React Native
-                {
-                    moduleName: '@env',
-                    path: '.env',
-                },
-            ],
-        ],
-    };
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ['module:react-native-dotenv'],
+  };
 };
